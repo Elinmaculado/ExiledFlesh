53fec2c77f1cc3d3a64f713345af9d23f4552297
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2024-10-18 11:31:32 -0600

    Tank controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidadMovimiento = 5f; // Velocidad de avance y retroceso
+    public float velocidadRotacion = 100f; // Velocidad de rotación 
+
+    private void Update()
+    {
+        // Movimiento hacia adelante y atrás 
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * velocidadMovimiento * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            transform.Translate(-Vector3.forward * velocidadMovimiento * Time.deltaTime);
+        }
+
+        // Rotación a la izquierda y derecha
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -velocidadRotacion * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, velocidadRotacion * Time.deltaTime);
+        }
+    }
+}
