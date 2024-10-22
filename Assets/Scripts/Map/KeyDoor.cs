b1af27082576a826c70905376f98346976db21cc
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2024-10-22 10:31:51 -0600

    Key/Door system done

---
+++ Assets/Scripts/Map/KeyDoor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoor : MonoBehaviour
+{
+    [SerializeField] private Key.KeyType doorKey;
+
+    public Key.KeyType GetKeyType()
+    {
+        return doorKey; 
+    }
+
+    public void OpenDoor()
+    {
+        gameObject.SetActive(false);
+    }
+}
